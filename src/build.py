f1a9fea8038079304eb8fcc8b987054cc5b2b882
imhet <me@het.im>
2021-03-08 01:23:48 +0800

    fix blog feed parse error

--- src/build.py
+++ src/build.py
@@ -122,7 +122,7 @@ def fetch_blog_entries():
         {
             "title": entry["title"],
             "url": entry["link"].split("#")[0],
-            "published": entry["published"].split("T")[0],
+            "published": entry["updated"].split("T")[0],
         }
         for entry in entries
     ]
