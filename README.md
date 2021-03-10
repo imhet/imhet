184bf5757e4ef8835e93ad09e0fe6dafdd44d71a
imhet <me@het.im>
2021-03-10 22:40:56 +0800

    add pic

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Hi，我是 imhet，目前在深圳某厂做 Android 开发，喜欢开源分享，以下内容通过 <a href="https://github.com/imhet/imhet/actions" target="_blank">imhet/actions</a> 每日自动更新。
 
+![](https://cn.bing.com/th?id=OHR.NotoriousRBG_EN-US0765557260_UHD.jpg)
+
 <table width="800px">
 <tr>
 <td valign="top" width="50%">
--- src/build.py
+++ src/build.py
@@ -1,7 +1,5 @@
 from python_graphql_client import GraphqlClient
 import feedparser
-import httpx
-import json
 import pathlib
 import re
 import os
@@ -9,7 +7,6 @@ import datetime
 
 root = pathlib.Path(__file__).parent.resolve()
 client = GraphqlClient(endpoint="https://api.github.com/graphql")
-
 
 TOKEN = os.environ.get("GH_TOKEN", "")
 
@@ -24,149 +21,37 @@ def replace_chunk(content, marker, chunk, inline=False):
     chunk = "<!-- {} starts -->{}<!-- {} ends -->".format(marker, chunk, marker)
     return r.sub(chunk, content)
 
-def formatGMTime(timestamp):
-    GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
-    dateStr = datetime.datetime.strptime(timestamp, GMT_FORMAT) + datetime.timedelta(hours=8)
-    return dateStr.date()
 
-def make_query(after_cursor=None):
-    return """
-query {
-  viewer {
-    repositories(first: 100, privacy: PUBLIC, after:AFTER) {
-      pageInfo {
-        hasNextPage
-        endCursor
-      }
-      nodes {
-        name
-        description
-        url
-        releases(last:1) {
-          totalCount
-          nodes {
-            name
-            publishedAt
-            url
-          }
-        }
-      }
-    }
-  }
-}
-""".replace(
-        "AFTER", '"{}"'.format(after_cursor) if after_cursor else "null"
-    )
+def format_date(timestamp):
+    return datetime.datetime.strptime(timestamp, '%a, %d %b %Y %H:%M:%S GMT') + datetime.timedelta(hours=8).date()
 
-
-def fetch_releases(oauth_token):
-    repos = []
-    releases = []
-    repo_names = set()
-    has_next_page = True
-    after_cursor = None
-
-    while has_next_page:
-        data = client.execute(
-            query=make_query(after_cursor),
-            headers={"Authorization": "Bearer {}".format(oauth_token)},
-        )
-        print()
-        print(json.dumps(data, indent=4))
-        print()
-        for repo in data["data"]["viewer"]["repositories"]["nodes"]:
-            if repo["releases"]["totalCount"] and repo["name"] not in repo_names:
-                repos.append(repo)
-                repo_names.add(repo["name"])
-                releases.append(
-                    {
-                        "repo": repo["name"],
-                        "repo_url": repo["url"],
-                        "description": repo["description"],
-                        "release": repo["releases"]["nodes"][0]["name"]
-                        .replace(repo["name"], "")
-                        .strip(),
-                        "published_at": repo["releases"]["nodes"][0][
-                            "publishedAt"
-                        ].split("T")[0],
-                        "url": repo["releases"]["nodes"][0]["url"],
-                    }
-                )
-        has_next_page = data["data"]["viewer"]["repositories"]["pageInfo"][
-            "hasNextPage"
-        ]
-        after_cursor = data["data"]["viewer"]["repositories"]["pageInfo"]["endCursor"]
-    return releases
-
-
-def fetch_code_time():
-    return httpx.get(
-        "https://gist.githubusercontent.com/tw93/7854aac61f991ef4e7ae7b8440e4fdc6/raw/"
-    )
 
 def fetch_douban():
-    entries = feedparser.parse("https://www.douban.com/feed/people/heyitao/interests")["entries"]
     return [
         {
             "title": item["title"],
             "url": item["link"].split("#")[0],
-            "published": formatGMTime(item["published"])
+            "published": format_date(item["published"])
         }
-        for item in entries
+        for item in feedparser.parse("https://www.douban.com/feed/people/heyitao/interests")["entries"]
     ]
 
 
 def fetch_blog_entries():
-    entries = feedparser.parse("https://heyitao.com/feed")["entries"]
     return [
         {
             "title": entry["title"],
             "url": entry["link"].split("#")[0],
             "published": entry["updated"].split("T")[0],
         }
-        for entry in entries
+        for entry in feedparser.parse("https://heyitao.com/feed")["entries"]
     ]
 
 
 if __name__ == "__main__":
     real_root = pathlib.Path(__file__).parent.parent.resolve()
     readme = real_root / "README.md"
-    
-    # 发布的项目
-    project_releases = root / "release-projects.md"
-    releases = fetch_releases(TOKEN)
-    releases.sort(key=lambda r: r["published_at"], reverse=True)
-    md = "\n".join(
-        [
-            "* <a href='{url}' target='_blank'>{repo} {release}</a> - {published_at}".format(**release)
-            for release in releases[:8]
-        ]
-    )
     readme_contents = readme.open().read()
-    # rewritten = replace_chunk(readme_contents, "recent_releases", md)
-
-    # github 仓库更新统计
-    project_releases_md = "\n".join(
-        [
-            (
-                "* **[{repo}]({repo_url})**: [{release}]({url}) - {published_at}\n"
-                "<br>{description}"
-            ).format(**release)
-            for release in releases
-        ]
-    )
-    project_releases_content = project_releases.open().read()
-    project_releases_content = replace_chunk(
-        project_releases_content, "recent_releases", project_releases_md
-    )
-    project_releases_content = replace_chunk(
-        project_releases_content, "release_count", str(len(releases)), inline=True
-    )
-    project_releases.open("w").write(project_releases_content)
-
-    code_time_text = "\n```text\n"+fetch_code_time().text+"\n```\n"
-
-    # rewritten = replace_chunk(rewritten, "code_time", code_time_text)
 
     # 豆瓣   
     doubans = fetch_douban()[:10]
